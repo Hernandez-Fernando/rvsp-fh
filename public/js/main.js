dab68cc7641ed28f4c448fe8c253217a40e3e164
Fernando Hernandez <chivista3001@icloud.com>
2020-12-09 07:25:35 -0700

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ function searchReservation() {
     let code = $('#number').val()
 
     $.post("/reservation", {code:code}, function(data) {
-        let outcome = `<div class="container"><p><span class="text-bold">Name:</span> ${data[0].guestname} ${data[0].guestlastname}</p><p><span class="text-bold">Number of Guests:</span> ${data[0].guestnumber}</p>`
+        let outcome = `<label class="font-weight-medium">Your Reservation</label><p><span class="font-weight-medium">Name:</span> ${data[0].guestname} ${data[0].guestlastname}</p><p><span class="font-weight-medium">Number of Guests:</span> ${data[0].guestnumber}</p>`
 
         $('#result').html(outcome)
     })
