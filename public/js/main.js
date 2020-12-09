6ca99d42d1d954f7662601b9625afb7b8a3f4ab5
Fernando Hernandez <chivista3001@icloud.com>
2020-12-08 23:17:40 -0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,8 @@ function searchReservation() {
     let code = $('#number').val()
 
     $.post("/reservation", {code:code}, function(data) {
-        console.log(data)
+        let outcome = `<div class="container><p class="text-bold>${data[guestname]} ${data[guestlastname]}</p><p>Number of Guests: ${data[guestnumber]}</p>`
+
+        $('#result').html(outcome)
     })
 }
